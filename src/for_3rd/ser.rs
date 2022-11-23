16036a5b1aeade295795d530870fd2ba25df2971
Aster <galaster@foxmail.com>
2022-11-23 16:48:00 +0800

    Add basic support for `Serialize` + `Deserialize`

---
+++ src/for_3rd/ser.rs
@@ -0,0 +1,32 @@
+use serde::ser::Error;
+use serde::{Serialize, Serializer};
+
+use crate::{BigFloatNumber, Radix, RoundingMode};
+
+impl Serialize for BigFloatNumber {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self.format(Radix::Dec, RoundingMode::None) {
+            Ok(s) => serializer.serialize_str(&s),
+            Err(e) => Err(Error::custom(format!("{e:?}"))),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::to_string;
+
+    use crate::BigFloatNumber;
+
+    #[test]
+    fn to_json() {
+        assert_eq!(
+            to_string(&BigFloatNumber::new(0).unwrap()).unwrap(),
+            "\"0.0\""
+        );
+        assert_eq!(
+            to_string(&BigFloatNumber::from_f32(64 + 1, 0.3).unwrap()).unwrap(),
+            "\"3.00000011920928955078125e-1\""
+        );
+    }
+}
